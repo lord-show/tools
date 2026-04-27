8b059225f564f514a00c881bc0f1280914671137
IAmTheDoctor8 <egor-yoda@yandex.ru>
2026-04-27 19:02:32 +0300

    Add up, down scripts

---
+++ down.sh
@@ -0,0 +1,5 @@
+# Запуск Portainer
+cd Portainer && docker compose down
+
+# Запуск Nextcloud
+cd ../Nextcloud && docker compose down
---
+++ up.sh
@@ -0,0 +1,5 @@
+# Запуск Portainer
+cd Portainer && docker compose up -d
+
+# Запуск Nextcloud
+cd ../Nextcloud && docker compose u
